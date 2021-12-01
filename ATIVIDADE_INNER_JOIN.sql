baa5fd714057f1edb63eee7cb72c697e001d478e
BlVck213 <paulocesarrrpontes@gmail.com>
2021-12-01 11:26:58 -0300

    Atividade Inner Join :white_check_mark:

--- ATIVIDADE_INNER_JOIN.sql
+++ ATIVIDADE_INNER_JOIN.sql
@@ -2,16 +2,29 @@ use dbfilmes;
 
 # NO WHERE
 select tblFilme.nome as nomeFilme, tblFilme.nomeOriginal, tblclassificacao.nome as nomeClassificacao, tblGenero.nome as nomeGenero, tblAtor.nome as nomeAtor 
-       from tblFilme, tblGenero, tblfilme_genero, tblclassificacao, tblator
-       where tblFilme.idFilme = tblfilme_genero.idFilme and
-       tblGenero.idGenero = tblfilme_genero.idGenero and tblFilme.idClassificacao = tblclassificacao.idClassificacao and tblAtor.dataNascimento >= "1940-01-01"
+       from tblFilme, tblGenero, tblfilme_genero, tblclassificacao, tblator, tblfilme_ator
+       where tblFilme.idFilme = tblfilme_genero.idFilme and 
+       tblfilme_ator.idAtor = tblator.idAtor and 
+       tblFilme.idFilme = tblfilme_ator.idFilme and
+       tblGenero.idGenero = tblfilme_genero.idGenero and 
+       tblFilme.idClassificacao = tblclassificacao.idClassificacao and 
+       tblAtor.dataNascimento >= "1940-01-01"
        order by nomeAtor;
        
 # NO JOIN
-select tblFilme.nome as nomeFilme, tblFilme.nomeOriginal, tblclassificacao.nome as nomeClassificacao, 
-	   tblGenero.nome as nomeGenero, tblAtor.nome as nomeAtor from tblFilme 
+select tblFilme.nome as nomeFilme, tblFilme.nomeOriginal, 
+	   tblclassificacao.nome as nomeClassificacao, 
+	   tblGenero.nome as nomeGenero, tblAtor.nome as nomeAtor 
+       from tblFilme 
        inner join tblFilme_Genero on tblFilme.idFilme = tblfilme_genero.idFilme 
+       inner join tblfilme_ator on tblFilme.idFilme = tblfilme_ator.idFilme
        inner join tblGenero on tblGenero.idGenero = tblfilme_genero.idGenero 
        inner join tblclassificacao on tblFilme.idClassificacao = tblclassificacao.idClassificacao 
-       inner join tblAtor on tblAtor.dataNascimento >= "1940-01-01"
+       inner join tblAtor on tblAtor.dataNascimento >= "1940-01-01" and tblfilme_ator.idAtor = tblator.idAtor
        order by nomeAtor;      
+       
+       
+       
+      git add .
+      git commit -m "Atividade Inner Join :white_check_mark:"
+      git push origin main
